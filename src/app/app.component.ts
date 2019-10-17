76252543efc14469489944c49422ee3ee622837e
Leonardo Barajas <leonardo.barajas@thomsonreuters.com>
2019-10-17 17:12:01 -0500

    components
    create the  first components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl: './header/header.component.html',
+  styleUrls: ['./app.component.sass']
+})
+@Component({
+  selector: 'app-root',
+  templateUrl: './footer/footer.component.html',
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
